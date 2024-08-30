5f36ed4cbbb875a0ff98c126965e6ba14cedacfd
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-30 12:10:23 -0700

    cmp - add validation

---
+++ validation/Frame/test.tcl
@@ -0,0 +1,11 @@
+wipe; source test_BeamLoad3D.tcl
+wipe; source test_BeamsAndSections3D.tcl
+wipe; source test_ShearBeam3D.tcl
+wipe; source  AISC25_3D.tcl
+
+#wipe; source test_section.tcl
+
+# wipe; source test_EigenFrame.tcl
+# wipe; source test_BeamLoad2D.tcl
+# wipe; source test_BeamsAndSections2D.tcl
+# wipe; source test_ShearBeam2D.tcl
